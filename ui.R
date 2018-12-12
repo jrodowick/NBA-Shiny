776f13042ba40498f2329c03faaa814f0bfce850
jrodowick <jrodowick@mail.csuchico.edu>
2018-12-12 13:55:14 -0800

    Adds tabs for NBA/Fantasy/Adds team list selector

--- ui.R
+++ ui.R
@@ -3,12 +3,48 @@
 library(shiny)
 library(shinydashboard)
 
+teams <- list(
+  "Atlanta Hawks",
+  "Boston Celtics",
+  "Brooklyn Nets",
+  "Charlotte Hornets",
+  "Chicago Bulls",
+  "Cleveland Cavaliers",
+  "Dallas Mavericks",
+  "Denver Nuggets",
+  "Detroit Pistons",
+  "Golden State Warriors",
+  "Houston Rockets",
+  "Indiana Pacers",
+  "Los Angeles Clippers",
+  "Los Angeles Lakers",
+  "Memphis Grizzlies",
+  "Miami Heat",
+  "Milwaukee Bucks",
+  "Minnesota Timberwolves",
+  "New Orleans Pelicans",
+  "New York Knicks",
+  "Oklahoma City Thunder",
+  "Orlando Magic",
+  "Philadelphia 76ers",
+  "Phoenix Suns",
+  "Portland Trail Blazers",
+  "Sacramento Kings",
+  "San Antonio Spurs",
+  "Toronto Raptors",
+  "Utah Jazz",
+  "Washington Wizards"
+)
+
+
 dashboardPage(
   dashboardHeader(
-    title=""
+    title="Stats Dashboard"
   ),
   dashboardSidebar(
-    img(id="logo", src="NBA-logo.jpg")
+    img(id="logo", src="NBA-logo.jpg"),
+    actionButton(inputId="scrapeNBA", label="Scrape NBA stats", class="text-center"),
+    actionButton(inputId="scrapeFantasy", label="Scrape Fantasy stats")
   ),
   dashboardBody(
     tags$head(
@@ -18,11 +54,17 @@ dashboardPage(
         href="style.css"
       )
     ),
-  h1("NBA stat scraper/Fantasy Basketball analyzer"),
-  tags$hr(),
-  actionButton(inputId="scrapeNBA", label="Scrape NBA stats"),
-  actionButton(inputId="scrapeFantasy", label="Scrape Fantasy stats")
-  
+    h1("NBA stat scraper/Fantasy Basketball analyzer"),
+    tabsetPanel(
+      tabPanel(
+        h4("NBA Stats/Graphs"),
+        selectInput(inputId="teamChoice",
+                    label="Select Team",
+                    choices = teams)),
+      tabPanel(
+        h4("Fantasy Basketball Analysis")
+      )
+    )
   )
 )
 
