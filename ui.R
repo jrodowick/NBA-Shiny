9acf9a6a0fa78cc6c34f7f3841e686585c342935
jrodowick <jrodowick@mail.csuchico.edu>
2018-12-11 18:14:32 -0800

    Adds a basic dashboard/heading/and buttons

--- ui.R
+++ ui.R
@@ -1,7 +1,29 @@
 #ui.R
 
 library(shiny)
-fluidPage(
-  "Hello World"
+library(shinydashboard)
+
+dashboardPage(
+  dashboardHeader(
+    title=""
+  ),
+  dashboardSidebar(
+    img(id="logo", src="NBA-logo.jpg")
+  ),
+  dashboardBody(
+    tags$head(
+      tags$link(
+        rel="stylesheet",
+        type="text/css",
+        href="style.css"
+      )
+    ),
+  h1("NBA stat scraper/Fantasy Basketball analyzer"),
+  tags$hr(),
+  actionButton(inputId="scrapeNBA", label="Scrape NBA stats"),
+  actionButton(inputId="scrapeFantasy", label="Scrape Fantasy stats")
+  
+  )
 )
+
 
