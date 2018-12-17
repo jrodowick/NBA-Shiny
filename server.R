952443a2df48f0f551509e6b3ea867dbfcd98d7f
jrodowick <jrodowick@mail.csuchico.edu>
2018-12-17 14:03:30 -0800

    Displays table, adds margin

--- server.R
+++ server.R
@@ -1,5 +1,20 @@
 #server.R
+library(ggplot2)
+library(stringr)
 
 function(input,output) {
-  
+  output$team <- renderText({
+    input$teamChoice
+  })
+  output$table <- renderTable({
+    df <- read.csv(str_c(input$teamChoice,'.csv'))
+  })
+  observeEvent(input$scrapeNBA, {
+    showModal(modalDialog(
+      title = h2('Web scrape initiated'),
+      h3('Scraping ESPN.com/NBA ...\n'),
+      h3('Updating .csv files')
+    ))
+    source('scrapeNBA.R')
+  })
 }
